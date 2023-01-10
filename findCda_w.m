1a2c4feab92cc1fe9e1b2433742554127464d1b3
jhoyng <jhoyng@student.tudelft.nl>
2023-01-10 17:59:53 +0100

    CD zonder vleugel bepaald met L/D = 16 (staat in reader) Viscous analysis gebruikt. CD_nowing = 0.0068

--- findCda_w.m
+++ findCda_w.m
@@ -1,7 +1,6 @@
 % Wing planform geometry 
 %                x    y     z   chord(m)    twist angle (deg) 
 AC.Wing.Geom = [0     0     0     6.05         0;
-                2.16  4.79  0.698     4.16         0;
                 5.74  14.65   1.184     1.48         0];
 
 % Wing incidence angle (degree)
@@ -16,7 +15,7 @@ AC.Wing.Airfoils   =    [0.2171    0.3450    0.2975    0.2685    0.2893    -0.12
 AC.Wing.eta = [0;1];  % Spanwise location of the airfoil sections
 
 % Viscous vs inviscid
-AC.Visc  = 0;              % 0 for inviscid and 1 for viscous analysis
+AC.Visc  = 1;              % 0 for inviscid and 1 for viscous analysis
 AC.Aero.MaxIterIndex = 150;    %Maximum number of Iteration for the
                                 %convergence of viscous calculation
                  
@@ -30,7 +29,9 @@ T_cruise = 218.808;
 Re = rho* meanChord*V_Cruise/viscosity;
 %AoA = params.AoA;
 a = sqrt(1.4*287*T_cruise);
-
+CruiseLD = 16;
+Cruiseweight = 38780;                    %Nu MTOW gepakt
+Wingarea = 93.50;
                                 
 % Flight Condition
 AC.Aero.V     = V_Cruise;            % flight speed (m/s)
@@ -38,7 +39,7 @@ AC.Aero.rho   = rho;                    % air density  (kg/m3)
 AC.Aero.alt   = alt;                    % flight altitude (m)
 AC.Aero.Re    = Re;                     % reynolds number (bqased on mean aerodynamic chord)
 AC.Aero.M     = V_Cruise/a;          % flight Mach number 
-AC.Aero.CL    = 0.4;                    % lift coefficient - comment this line to run the code for given alpha%
+AC.Aero.CL    = 2*9.81*Cruiseweight/(rho*(V_Cruise^2)*Wingarea);                    % lift coefficient - comment this line to run the code for given alpha%
 %AC.Aero.Alpha = params.AoA;            % angle of attack -  comment this line to run the code for given cl 
 
 
@@ -47,14 +48,9 @@ AC.Aero.CL    = 0.4;                    % lift coefficient - comment this line t
 tic
 
 Res = Q3D_solver(AC);
-disp(Res.Wing.cl);
 toc
 
 %doesnt output load but outputs only Cl. This first has to be multiplied by
 %the root and q in order to resemble the load so for now it is still wrong.
+CD_nowing = (Res.CLwing/CruiseLD)-Res.CDwing; % Value CD_nowing = 0.0068
 
-loadVector = [Res.Wing.Yst Res.Wing.cl]; 
-
-
-
-end
