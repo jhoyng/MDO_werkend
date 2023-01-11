06ec27075ccc990c8c8d7fdac4c4609a15625615
jhoyng <jhoyng@student.tudelft.nl>
2023-01-11 12:06:20 +0100

    Aero weights toegoegd aan design vector en ook in master design vector gestopt (x(29) en x(30))

--- Aerodynamics.m
+++ Aerodynamics.m
@@ -1,16 +1,18 @@
 function [Aero_LD] = Aerodynamics(x)
 
-%x0 = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweet_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5)];
+%x0 = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweet_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5)  W_mtow_aero  W_fuel_aero];
 
 %from design vector
 Chord_root = x(1);
 Taper_mid = x(2) ;
 Incidence_root = x(3);
 Incidence_mid = x(4);
-Incidence_tip = x(8);
 Taper_tip = x(5);
 Span_tip = x(6);
 Sweep_LE_tip = x(7);
+Incidence_tip = x(8);
+W_mtow = x(29);
+W_fuel = x(30);
 
 %Fixed values from reference planform
 TE_sweep_mid = 4.6*(pi/180);            
@@ -48,9 +50,9 @@ AC.Wing.inc  = 0;
 
 %x0 = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweet_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5)];
 % Airfoil coefficients input matrix
-%                    | ->     upper curve coeff.                    <-|       | ->       lower curve coeff.             <-| 
-AC.Wing.Airfoils   =    [AuR(1)    AuR(2)    AuR(3)    AuR(4)    AuR(5)        AlR(1)   AlR(2)   AlR(3)   AlR(4)    AlR(5);
-                         AuT(1)    AuT(2)    AuT(3)    AuT(4)    AuT(5)        AlT(1)   AlT(2)   AlT(3)   AlT(4)    AlT(5)];
+%                    | ->     upper curve coeff.              <-|       | ->       lower curve coeff.       <-| 
+AC.Wing.Airfoils   =    [x(9)    x(10)    x(11)    x(12)    x(13)        x(14)   x(15)   x(16)   x(17)    x(18);
+                         x(19)   x(20)    x(21)    x(22)    x(23)        x(24)   x(25)   x(26)   x(27)    x(28)];
                   
 
 AC.Wing.eta = [0;1];                    % Spanwise location of the airfoil sections
@@ -74,6 +76,7 @@ Cruiseweight = 38780;                   %Functie voor design point uit assignmen
 Wingarea = ((Chord_root+Chord_mid)/2)*Span_mid+((Chord_mid+Chord_tip)/2)*Span_tip;                        
 meanChord = (2/Wingarea)*((Chord_root*Span_mid-(0.5*((Chord_root-Chord_mid)/Span_mid)*Span_mid^2))+(Chord_mid*Span_tip-(0.5*((Chord_mid-Chord_tip)/Span_tip)*Span_tip^2)));                        %Mean aerodynamic chord?
 Re = rho* meanChord*V_Cruise/viscosity;
+L_des = sqrt(W_mtow*(W_mtow-W_fuel));
 
 % Flight Condition
 AC.Aero.V     = V_Cruise;               % flight speed (m/s)
@@ -81,7 +84,7 @@ AC.Aero.rho   = rho;                    % air density  (kg/m3)
 AC.Aero.alt   = alt;                    % flight altitude (m)
 AC.Aero.Re    = Re;                     % reynolds number (bqased on mean aerodynamic chord)
 AC.Aero.M     = V_Cruise/a;             % flight Mach number 
-AC.Aero.CL    = 2*9.81*Cruiseweight/(rho*(V_Cruise^2)*Wingarea);                    % lift coefficient - comment this line to run the code for given alpha%
+AC.Aero.CL    = 2*L_des/(rho*(V_Cruise^2)*Wingarea);                    % lift coefficient - comment this line to run the code for given alpha%
 %AC.Aero.Alpha = params.AoA;            % angle of attack -  comment this line to run the code for given cl 
 
 
