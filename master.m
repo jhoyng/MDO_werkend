b80c66bd74c164f25b08778a61c3e9cee407c170
Guillaumevandijk <84512311+Guillaumevandijk@users.noreply.github.com>
2023-01-12 09:40:09 +0100

    Add files via upload
    correctie loads en master

--- master.m
+++ master.m
@@ -27,13 +27,14 @@ sweep1_TE   =   4.60;
 Rootchord_0 = 5.78 ;
 Taper_mid_0 = 0.699;
 Taper_tip_0 = 0.356;
-Root_twist_0 = 3;
-Mid_twist_0 = 2;
-Tip_twist_0 = 0;
+Root_twist_0 = 4;
+Mid_twist_0 = 2.62;
+Tip_twist_0 = -0.44;    
 Tip_span_0 = 14.038 ;
 LE_sweep_tip_0 = 19.37 ;
-W_mtow =  43090;         %[kg]
+W_mtow_0 =  43090;         %[kg]
 W_zerofuel =  35830;         %[kg]
+W_fuel_0 = 13365*0.81715;           %[m^3]*[kg/m^3] = [kg]
 
 %Defign the root and tip airfoil
 %e553
@@ -47,8 +48,16 @@ AlT = [-0.1299   -0.05   -0.1635   -0.0476    0.0797];
 
 
 
-x0 = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweep_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) W_mtow W_zerofuel];
+x0 = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweep_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) W_mtow_0 W_fuel_0];
+%x0 =[    1             2           3              4          5            6              7             8        9       10      11      12      13      14      15      16      16      17      18      19     20       21      22      23      24      25      26     26        27      28   ]; 
 
+%Creating bounds for the design variables
+%Upper bounds
+%ub = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweep_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) W_mtow_0 W_fuel_0];
+ub = [6.5                1            6       6             0.5         16          25              4           AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) 3000     6000];
 
+%Lower bounds
+%lb = [Rootchord_0  Taper_mid_0  Root_twist_0 Mid_twist_0  Taper_tip_0  Tip_span_0  LE_sweep_tip_0  Tip_twist_0 AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) W_mtow_0 W_fuel_0];
+lb = [5                 0.5             -6    -6           0.2          12          10              -4          AuR(1)  AuR(2)  AuR(3)  AuR(4)  AuR(5)  AlR(1)  AlR(2)  AlR(3)  AlR(4)  AlR(5)  AuT(1)  AuT(2)  AuT(3)  AuT(4)  AuT(5)  AlT(1)  AlT(2)  AlT(3)  AlT(4)  AlT(5) 6000     160000];
 
-
+loads(x0);
